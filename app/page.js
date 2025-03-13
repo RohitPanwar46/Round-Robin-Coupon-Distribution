4c7a00c6f95d3c3ea337d310dee0e3f851561131
Rohit <freelancerrohit46@gmail.com>
2025-03-13 15:51:15 +0530

    some changes

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="text-black flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-3xl font-bold mb-4">Round-Robin Coupon Distribution</h1>
       <button
         onClick={claimCoupon}
